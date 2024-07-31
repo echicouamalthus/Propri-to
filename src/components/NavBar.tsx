89761d7412fed32fedd7825c01c9c23dfaa0e9c4
Echicoua Malthus <e.malthus@asnumeric.com>
2024-07-31 23:55:42 +0000

    build landing page for proprio-to

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,82 @@
+'use client'
+
+import React, { useState } from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+
+import LogoSvgIcon from '../../public/proprio-to.svg'
+import { link, Links } from '@/lib/constante'
+import { Button } from './ui/button'
+import { MenuIcon, X } from 'lucide-react'
+import { useBreakpoint } from '@/lib/hook/tailwind'
+import clsx from 'clsx'
+
+export default function NavBar() {
+  const [ouvrir, setOuvrir] = useState(true)
+  const desktop = useBreakpoint('md')
+
+  return (
+    <nav className='sticky top-0 bg-white'>
+      <div className='mx-auto max-w-6xl items-center justify-between space-y-3 p-2 py-2 lg:flex lg:space-y-0'>
+        <div className='flex w-full max-w-4xl items-center justify-between'>
+          <div className='flex w-full items-center gap-2'>
+            <Image
+              src={LogoSvgIcon}
+              alt='logo SVG'
+              width={desktop ? 60 : 40}
+              height={desktop ? 60 : 40}
+            />
+            <span className='font-mono text-xl font-bold'>Proprio-to</span>
+          </div>
+
+          <div className='flex justify-center lg:block lg:w-full'>
+            <ul
+              className={clsx(
+                'absolute left-0 top-28 -z-20 flex w-full flex-col gap-4 bg-white p-4 transition-all duration-100 ease-in-out lg:static lg:h-auto lg:flex-row lg:bg-transparent lg:p-0',
+                {
+                  '-top-32': ouvrir === false
+                }
+              )}
+            >
+              {Links.map((e: link) => {
+                return (
+                  <Link
+                    key={`${e.label}`}
+                    href={e.href}
+                    className='underline-offset-4 hover:underline'
+                  >
+                    {e.label}
+                  </Link>
+                )
+              })}
+            </ul>
+
+            <Button
+              size={'icon'}
+              variant={'ghost'}
+              className='block p-0 lg:hidden'
+            >
+              {ouvrir ? (
+                <X onClick={() => setOuvrir(prevOuvrir => !prevOuvrir)} />
+              ) : (
+                <MenuIcon
+                  onClick={() => setOuvrir(prevOuvrir => !prevOuvrir)}
+                />
+              )}
+            </Button>
+          </div>
+        </div>
+
+        <div className='flex items-center lg:space-x-4'>
+          <Button asChild variant={'link'} className='hidden lg:block'>
+            <Link href={'#'}>se connecter</Link>
+          </Button>
+
+          <Button variant={'default'} className='w-full lg:w-auto'>
+            <Link href={'#'}>Essai Gratuit</Link>
+          </Button>
+        </div>
+      </div>
+    </nav>
+  )
+}
---
+++ src/lib/constante.ts
@@ -0,0 +1,19 @@
+export type link = {
+  label: string
+  href: string
+}
+
+export const Links: link[] = [
+  {
+    label: 'Pour qui ?',
+    href: '#'
+  },
+  {
+    label: 'Fonctionnalité',
+    href: '#'
+  },
+  {
+    label: 'contact',
+    href: '#'
+  }
+]
